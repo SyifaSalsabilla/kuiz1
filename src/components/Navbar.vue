846c010994873bee9049baeab5393c13fe0d305b
SyifaSalsabilla <12050120344@students.uin-suska.ac.id>
2023-04-18 00:00:35 +0700

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="/src/assets/pg.jpeg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        My Profile Web
+      </a>
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <router-link id="home" class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link id="profile" class="nav-link" to="/profile">Profile</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link id="about" class="nav-link" to="/tentang">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link id="about" class="nav-link" to="/help">Travel</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navbar"
+};
+</script>
+
+
---
+++ src/views/Tentang.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets/osis5.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Pertukaran Pengurusan Osis 2019</h5>
+          <p>Pertukaran Pengurus Osis Dengan Osis 2019 Dilakukan Setiap Masa Jabatan Selesai.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/k5.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Milad Teknik Informatika</h5>
+          <p>Kepanitian Acara Milad Teknik Informatika Yang Ke-23 Th,Dimana Acara Ini Diadakan Setiap Tahunnya.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/k2.jpeg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Porseni TIF Yang Ke 3</h5>
+          <p>Pekan Olahraga dan Seni Teknik Informatika Ini Adalah Kegiatan Mahasiswa TIF Yang Diadakan Setiap Tahunnya.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tentang"
+}
+</script>
+
+<style scoped>
+
+</style>
